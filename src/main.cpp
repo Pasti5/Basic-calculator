db2dde5b3e788d6f61a5fc13f1c7bf1e696d6198
Pasti <220496727+Pasti5@users.noreply.github.com>
2026-08-25 10:18:38 +0200

    - Change the logic of how it works in those "cases" so it follows the refactorization of the whole program. - Add two variables "first_number" and "second"number" to each case so the order of asking user in a calculation operation (for example "multiply") is defined. - replace "exit(0)" with more reliable "return 0"

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,58 @@
 #include <iostream>
 
 #include "tools.h"
+#include "question_user.h"
 
 int main()
 {
     std::cout<<"\nWelcome to the basic calculator!\n";
     while(true) {
-        switch ( menu() ) {
-
+        switch (menu()) 
+        {
         case 1:
-            result( multiply() );
+            {
+            double first_number {getNumberFor("First factor")};
+            double second_number {getNumberFor("Second factor")};
+            printResult(multiply(first_number, second_number));
+            }
             break;
         case 2:
-            result( divide() );
+            {
+            double first_number {getNumberFor("Dividend")};
+            double second_number {getNumberFor("Divisor")};
+            printResult(divide(first_number, second_number));
+            }
             break;
         case 3:
-            result( add() );
+            {
+            double first_number {getNumberFor("First addend")};
+            double second_number {getNumberFor("Second addend")};
+            printResult(add(first_number, second_number));
+            }
             break;
         case 4:
-            result( substract() );
+            {
+            double first_number {getNumberFor("Minuend")};
+            double second_number {getNumberFor("Subtrahend")};
+            printResult(subtract(first_number, second_number));
+            }
             break;
         case 5:
-            result( power() );
+            {
+            double base {getNumberFor("Base")};
+            double exponent {getNumberFor("Exponent")};
+            printResult(power(base, exponent));
+            }
             break;
         case 6:
-            result( root() );
+            {
+            double radicand {getNumberFor("Radicand")};
+            double index {getNumberFor("Index")};
+            printResult(root(radicand, index));
+            }
             break;
         case 7:
-            exit(0);
+            return 0;
 
         default:
             break;
